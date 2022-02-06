5e29a9bb3956bf75b3c16421df99006d0e166605
sevvalkolemen <seval.kolemen@gmail.com>
2022-02-06 17:04:38 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Feature2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature2
+    {
+        [Key]
+        public int Feature2Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Iamge { get; set; }
+        public bool Status { get; set; }
+    }
+}
