963afce38c2ef00f2e459b09c0a4d752c242014d
sevvalkolemen <seval.kolemen@gmail.com>
2022-02-10 16:34:08 +0300

    Tur Rotaları Listelendi

---
+++ TraversalCoreProject/ViewComponents/Default/_PopulerDestinationsPartial.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _PopulerDestinationsPartial : ViewComponent
+    {
+        DestinationManager destinationManger = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManger.TGetList();
+            return View(values);
+        }
+    }
+}
