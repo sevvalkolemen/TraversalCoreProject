93166c62992677dccf1ec8ddb104954cc9867e68
sevvalkolemen <seval.kolemen@gmail.com>
2022-03-02 18:49:08 +0300

    Feature/Statics/SubAbout/Testimonial

---
+++ TraversalCoreProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+           // var values = featureManager.TGetList();
+           //ViewBag.image1=featureManager.
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Statistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using var context = new Context();
+            ViewBag.v1 = context.Destinations.Count();
+            ViewBag.v2 = context.Guides.Count();
+            ViewBag.v3 = 285;
+
+            return View();
+        }
+    }
+}
