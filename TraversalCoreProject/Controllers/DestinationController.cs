f6b21225537e8f2156e33f311851ed358ffb6e85
sevvalkolemen <seval.kolemen@gmail.com>
2022-04-29 00:30:11 +0300

    Layout Düzenlemeleri ve Single Page Sayfası

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,18 @@ namespace TraversalCoreProject.Controllers
         {
             var values = destinationManger.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
         }
     }
 }
